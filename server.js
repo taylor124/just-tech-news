d8ed4eddc461e5de9f1375b68084210c51dff5fd
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-03-10 20:54:34 -0500

    Added jest and tests

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const helpers = require('./utils/helpers');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 const session = require('express-session');
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
