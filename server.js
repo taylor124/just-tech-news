b2ffb06fed33a865b6ecff47b3fd1ebda711d965
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-03-10 20:08:17 -0500

    Added Post and Upvote feature

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session(sess));
 // turn on routes
 app.use(routes);
 
